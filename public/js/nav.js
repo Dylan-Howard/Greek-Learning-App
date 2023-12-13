33c60e84e6c9432d11cb2b858c89e1fbcd854b12
Dylan Howard <dylanhoward6@gamil.com>
2023-12-13 07:35:52 -0600

    Updated toggle options for navbar

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,6 +1,7 @@
-// eslint-disable-next-line node/no-unsupported-features/es-syntax
-import vocabMenu from "../js/vocab.js";
-import lessonsMenu from "../js/lessons.js";
+// eslint-disable-next-line node/no-unsupported-features/es-syntax, import/extensions
+import vocabMenu from "./vocab.js";
+// eslint-disable-next-line node/no-unsupported-features/es-syntax, import/extensions
+import lessonsMenu from "./lessons.js";
 
 const mainNav = () => {
   /* Attaches listener for click events on text units */
@@ -9,10 +10,30 @@ const mainNav = () => {
   /* On change, update `userLessons` based on checkbox */
   navIcons.forEach((el) =>
     el.addEventListener("click", () => {
+      /* Deactivate all other navbar icons */
+      Array.from(document.getElementsByClassName("nav-active"))
+        .filter((nav) => nav.id !== el.id)
+        .forEach((nav) => nav.classList.remove("nav-active"));
+      /* Deactivate all menu tabs */
+      Array.from(document.getElementsByClassName("menu-active"))
+        .filter((men) => men.id !== el.dataset.menu)
+        .forEach((men) => men.classList.remove("menu-active"));
+      /* Toggle navbar icon */
       if (el.classList.contains("nav-active")) {
         el.classList.remove("nav-active");
       } else {
         el.classList.add("nav-active");
+      }
+      /* Get target menu */
+      const targetMenu = document.getElementById(el.dataset.menu);
+      if (!targetMenu) {
+        return undefined;
+      }
+      /* Toggle target menu */
+      if (targetMenu.classList.contains("menu-active")) {
+        targetMenu.classList.remove("menu-active");
+      } else {
+        targetMenu.classList.add("menu-active");
       }
     }),
   );
