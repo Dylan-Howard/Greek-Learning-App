487a4224dbf72a6324f13032d06d942b64620c40
Dylan Howard <dylanhoward6@gamil.com>
2024-03-19 13:48:22 -0500

    Included local storage

--- src/Reader.tsx
+++ src/Reader.tsx
@@ -1,5 +1,6 @@
 import './Reader.css';
 import { useState } from 'react';
+// import { useCookies } from 'react-cookie';
 import fetchUser, { UserContext } from './User/User';
 import TextRenderer from './TextRenderer/TextRenderer';
 import Nav from './Nav/Nav';
@@ -12,32 +13,59 @@ const DEFAULT_CHAPTER_ID = 0;
 // eslint-disable-next-line no-console, @typescript-eslint/no-unused-vars
 const log = (message: any) => console.log(message);
 
+const tabs = [
+  { title: 'Home', iconName: 'home' },
+  { title: 'Lessons', iconName: 'play_lesson' },
+  { title: 'Dictionary', iconName: 'dictionary' },
+  { title: 'Settings', iconName: 'settings' },
+];
+
+const getUser = () => {
+  const localUser = localStorage.getItem('koineUser');
+
+  console.log(localUser);
+
+  return localUser ? JSON.parse(localUser) : fetchUser(TEST_USER_ID);
+};
+
+const saveUser = (userData: any) => {
+  localStorage.setItem('koineUser', userData);
+};
+
+// const clearUserData = () => {
+//   localStorage.clear();
+// };
+
 function App() {
   /* State for user details */
   const [activeUser, setActiveUser] = useState(
-    fetchUser(TEST_USER_ID),
+    getUser(),
   );
+  // const [userSettings, setUserSettings, removeUserSettings] = useCookies(['userSettings']);
+
   /* States for side bar */
   const [activeTabIndex, setActiveTabIndex] = useState(0);
   const [activeMorphologyId, setActiveMorphologyId] = useState(-1);
 
   /* States primarily for text rendering */
   /* @TODO - Merge these two states into a Page state { bookId, chapterId } */
-  const [activeBookId, setActiveBookId] = useState(DEFAULT_BOOK_ID);
-  const [activeChapterId, setActiveChapterId] = useState(DEFAULT_CHAPTER_ID);
-
-  const tabs = [
-    { title: 'Home', iconName: 'home' },
-    { title: 'Lessons', iconName: 'play_lesson' },
-    { title: 'Dictionary', iconName: 'dictionary' },
-    { title: 'Settings', iconName: 'settings' },
-  ];
+  const [ activeText , setActiveText ] = useState({
+    bookId: DEFAULT_BOOK_ID,
+    chapterId: DEFAULT_CHAPTER_ID,
+  });
+  // const [activeBookId, setActiveBookId] = useState(DEFAULT_BOOK_ID);
+  // const [activeChapterId, setActiveChapterId] = useState(DEFAULT_CHAPTER_ID);
 
   const changeActiveDeclension = (morphologyId: number) => {
     setActiveMorphologyId(morphologyId);
     if (morphologyId === -1) { setActiveTabIndex(0); }
     if (morphologyId !== -1 && activeTabIndex !== 4) { setActiveTabIndex(4); }
   };
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  // const handleUserUpdate = () => {
+  //   saveUser(activeUser);
+  // };
 
   if (activeMorphologyId !== -1) {
     tabs.push({ title: 'Details', iconName: 'chat_info' });
@@ -51,14 +79,16 @@ function App() {
           tabs={tabs}
           activeTabIndex={activeTabIndex}
           setActiveTabIndex={setActiveTabIndex}
-          activeChapterId={activeChapterId}
-          activeMorphologyId={activeMorphologyId}
+          // activeChapterId={activeChapterId}
+          // activeMorphologyId={activeMorphologyId}
+          activeText={activeText}
         />
         <TextRenderer
-          activeBookId={activeBookId}
-          setActiveBookId={setActiveBookId}
-          activeChapterId={activeChapterId}
-          setActiveChapterId={setActiveChapterId}
+          // activeBookId={activeBookId}
+          // setActiveBookId={setActiveBookId}
+          // activeChapterId={activeChapterId}
+          // setActiveChapterId={setActiveChapterId}
+          activeText={activeText}
           changeActiveDeclension={changeActiveDeclension}
         />
       </div>
