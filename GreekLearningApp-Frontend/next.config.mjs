e32e9ff50cfc51b7adcb6c63e27bebb23d3d04c1
Dylan Howard <dylanhoward6@gamil.com>
2024-10-02 20:47:19 -0500

    Updated deployment settings

--- GreekLearningApp-Frontend/next.config.mjs
+++ GreekLearningApp-Frontend/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: './dist', // Changes the build output directory to `./dist/`.
+  distDir: './build', // Changes the build output directory to `./dist/`.
 }
  
 export default nextConfig
